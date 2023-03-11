9bf6220d46f02ea6b37a3d4cd89b879967552242
ricardopresto <ricardopresto5@gmail.com>
2023-03-11 08:37:40 +0100

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,52 @@
-<script setup>
-  import Room from './components/Room.vue';
-</script>
-
 <template>
   <main class="main">
-
+    <div class="row" v-for="row in rows" :key="row.index">
+      <div class="column" v-for="column in columns" :key="column.index">
+        <Room :row="row" :column="column"/>
+      </div>
+    </div>
   </main>
 </template>
 
+<script>
+  import Room from './components/Room.vue';
+
+  export default {
+    components: {
+      Room
+    },
+    data() {
+      return {
+        rows: 3,
+        columns: 4,
+        layout: [],
+      }
+    },
+    mounted() {
+      this.update()
+    },
+    methods: {
+      update() {
+        for (let y = 1; y <= this.rows; y++) {
+          for (let x = 1; x <= this.columns; x++) {
+            this.layout.push({
+              index: [x, y],
+            })
+          }
+        }
+      }
+    }
+  }
+</script>
+
 <style scoped>
+.row {
+  display: flex;
+  flex-direction: row;
+}
+
+.column {
+  display: flex;
+  flex-direction: column;
+}
 </style>
--- src/components/Room.vue
+++ src/components/Room.vue
@@ -1,11 +1,48 @@
-<script setup>
-</script>
-
 <template>
   <main class="main">
-
+    <div class="inner">
+      <span>{{ row }}</span>
+      <span>{{ column }}</span>
+    </div>
   </main>
 </template>
 
+<script>
+  export default {
+    name: 'room',
+    props: {
+      row: {
+        type: Number,
+        required: true,
+      },
+      column: {
+        type: Number,
+        required: true,
+      }
+    },
+    data() {
+      return {
+        //
+      }
+    }
+  }
+</script>
+
 <style scoped>
+.main {
+  width: 200px;
+  height: 200px;
+  background-color: aqua;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 5px;
+  color: #fff;
+}
+
+.inner {
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+}
 </style>
